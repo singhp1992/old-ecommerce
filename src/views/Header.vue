2a9565fac044be905d2905c60f149c10fe18cd91
Preeti <singhp1992@gmail.com>
2019-08-28 20:54:51 -0500

    adding css to header comp

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -13,5 +13,23 @@
 </script>
 
 <style>
+#nav {
+  padding: 30px;
+  border: 1px solid black;
+  background-color: black;
+}
 
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  color: white;
+}
+
+a {
+  text-decoration: none;
+}
+
+#nav a.router-link-exact-active {
+  color: #d878f0;
+}
 </style>
